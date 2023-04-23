792b130f3d2a0cde08107e3eeb408faa34e4394b
Waldenylson Silva <waldenylson@gmail.com>
2023-04-23 00:17:39 -0300

    first commit

---
+++ app/root.tsx
@@ -0,0 +1,46 @@
+import type { LinksFunction, LoaderArgs } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+import { cssBundleHref } from "@remix-run/css-bundle";
+
+import { getUser } from "~/session.server";
+import tailwindStylesheetUrl from "~/styles/tailwind.css";
+
+import { ProSidebarProvider } from "react-pro-sidebar";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: tailwindStylesheetUrl },
+  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+];
+
+export const loader = async ({ request }: LoaderArgs) => {
+  return json({ user: await getUser(request) });
+};
+
+export default function App() {
+  return (
+    <html lang="en" className="h-full">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body className="h-full">
+        <ProSidebarProvider>
+          <Outlet />
+          <ScrollRestoration />
+          <Scripts />
+          <LiveReload />
+        </ProSidebarProvider>
+      </body>
+    </html>
+  );
+}
